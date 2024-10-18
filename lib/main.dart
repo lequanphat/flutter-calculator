2d65a949580d9c6ca2852dd97145464f21aef738
quanphatdev <lequanphat3579@gmail.com>
2024-10-18 23:28:38 +0700

    feat/widget-design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -27,37 +26,133 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  String username = '';
+  String password = '';
 
-  void generateNewWord() {
-    current = WordPair.random();
+  void updateUsernameInput(String value) {
+    username = value;
+    notifyListeners();
+  }
+
+  void updatePasswordInput(String value) {
+    password = value;
     notifyListeners();
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('A random idea:', style: TextStyle(fontSize: 24)),
-          SizedBox(height: 20),
-          Text(appState.current.asLowerCase, style: TextStyle(fontSize: 32)),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              appState.generateNewWord();
-            },
-            child: Text('Generate New Word'),
+        title: Container(
+          alignment: Alignment.center, // Căn giữa
+          child: Text(
+            'LOGIN',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ],
+        ),
+        toolbarHeight: 70, //
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  'assets/images/penguin.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Hello! Welcome back!',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                labelText: 'Enter username',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.text,
+              onChanged: (value) {
+                appState.updateUsernameInput(value);
+              },
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Enter password',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.visiblePassword,
+              onChanged: (value) {
+                appState.updatePasswordInput(value);
+              },
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Your Input'),
+                        content: Text(
+                          'Username: ${appState.username}\nPassword: ${appState.password}',
+                        ),
+                        actions: [
+                          TextButton(
+                            child: Text('OK'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Login'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
